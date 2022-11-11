a2951bb46e05c6efce563edc0ed8ef8aafc1abae
Hayk Sargsyan <hayksargsyan2019@bk.ru>
2022-11-11 17:01:18 +0400

    microservice-ws

--- mobile-app-api-users/src/main/java/com/example/mobileappapiusers/service/UserServiceImpl.java
+++ mobile-app-api-users/src/main/java/com/example/mobileappapiusers/service/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.mobileappapiusers.service;
 
-import com.example.mobileappapiusers.data.UserEntity;
-import com.example.mobileappapiusers.model.UserRest;
+
+import com.example.mobileappapiusers.data.AlbumsServiceClient;
+import com.example.mobileappapiusers.controller.dto.UserDto;
+import com.example.mobileappapiusers.entity.UserEntity;
+import com.example.mobileappapiusers.model.AlbumResponseModel;
 import com.example.mobileappapiusers.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,54 +20,85 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserRepository repository;
 
-    UserRepository userRepository;
+    //    private RestTemplate restTemplate;
+    private final Environment environment;
+    private AlbumsServiceClient albumsServiceClient;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public UserServiceImpl(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+    public UserServiceImpl(UserRepository repository, BCryptPasswordEncoder bCryptPasswordEncoder, Environment environment, AlbumsServiceClient albumsServiceClient) {
+        this.repository = repository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-
-        this.userRepository = userRepository;
+//
+        this.environment = environment;
+        this.albumsServiceClient = albumsServiceClient;
     }
 
     @Override
-    public UserRest createUser(UserRest userDto) {
+    public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+        userDto.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
         ModelMapper modelMapper = new ModelMapper();
+
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
-        userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
-        userRepository.save(userEntity);
 
-        return modelMapper.map(userEntity, UserRest.class);
 
+        return modelMapper.map(repository.save(userEntity), UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserDetailsByEmail(String email) {
+        UserEntity userEntity = repository.findByEmail(email);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(email);
+
+        }
+        return new ModelMapper().map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserDto getUserByUserId(String userId) {
+        UserEntity userEntity = repository.findByUserId(userId);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        ModelMapper modelMapper = new ModelMapper();
+
+        UserDto userDto = modelMapper.map(userEntity, UserDto.class);
+//        String albumsUrl=String.format(Objects.requireNonNull(environment.getProperty("albums.url")),userId);
+//        ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl,
+//                HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+//                });
+//        List<AlbumResponseModel> albumsList= albumsListResponse.getBody();
+        logger.info("Before calling albums Microservice");
+        List<AlbumResponseModel> albumsList = albumsServiceClient.getAlbums(userId);
+        logger.info("After calling albums Microservice");
+
+        userDto.setAlbums(albumsList);
+
+        return userDto;
 
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userRepository.findByEmail(username);
+        UserEntity userEntity = repository.findByEmail(username);
         if (userEntity == null) {
             throw new UsernameNotFoundException(username);
         }
-        return new User(userEntity.getEmail(),userEntity.getEncryptedPassword(), new ArrayList<>());
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true,
+                true, true, new ArrayList<>());
     }
-
-    @Override
-    public UserRest getUserDetailsByEmail(String email) {
-        UserEntity userEntity = userRepository.findByEmail(email);
-        if (userEntity == null) {
-            throw new UsernameNotFoundException(email);
-        }
-        return new ModelMapper().map(userEntity, UserRest.class) ;
-    }
-
-
 }
